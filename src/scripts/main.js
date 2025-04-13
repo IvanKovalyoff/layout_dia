4cae4c98a63d4e0a27a519f76cd831345903304b
Ivan <kovalevivan420@gmail.com>
2025-04-13 12:16:37 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const dropdowns = document.querySelector('.dropdown');
+const trigger = dropdowns.querySelector(`.dropdown__trigger`);
+
+trigger.addEventListener('click', () => {
+  dropdowns.classList.toggle('dropdown--active');
+});

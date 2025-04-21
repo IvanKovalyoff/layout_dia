7d725fdd966b7cae0a0bb985f7103d57e12ef113
Ivan <kovalevivan420@gmail.com>
2025-04-21 11:52:39 +0200

    new changes after review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,3 @@
 'use strict';
 
-const dropdown = document.querySelector('.dropdown');
-const trigger = dropdown.querySelector(`.dropdown__trigger`);
 
-trigger.addEventListener('click', () => {
-  dropdown.classList.toggle('dropdown--active');
-});
